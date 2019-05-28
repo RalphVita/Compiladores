36841b319729b603ddfb01e188d5897092f0f29d
Wanderson Ralph <ralph.vita@gmail.com>
2019-05-28 00:56:12 -0300

    Fim

---
+++ T3/menssagem.h
@@ -0,0 +1,22 @@
+#ifndef MENSSAGEM_H
+#define MENSSAGEM_H
+
+//Sucesso
+#define MSG_001 "PARSE SUCCESSFUL!"
+
+//Simbolo não reconhecido
+#define MSG_002 "SCANNING ERROR (%d): Unknown symbol %s\n"
+
+
+#define MSG_003 "SEMANTIC ERROR (%d): variable '%s' was not declared.\n"
+
+#define MSG_004 "SEMANTIC ERROR (%d): function '%s' was not declared.\n"
+
+#define MSG_005 "SEMANTIC ERROR (%d): variable '%s' already declared at line %d.\n"
+
+#define MSG_006 "SEMANTIC ERROR (%d): function '%s' already declared at line %d.\n"
+
+#define MSG_007 "SEMANTIC ERROR (%d): function '%s' was called with %d arguments but declared with %d parameters.\n"
+
+#endif // MENSSAGEM_H
+
