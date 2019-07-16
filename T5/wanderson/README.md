83da1b0d3f24a70809b8028cc3d19b0bf6e2f568
Wanderson Ralph <ralph.vita@gmail.com>
2019-07-15 21:11:13 -0300

    commit automático

--- T5/wanderson/README.md
+++
@@ -1,7 +0,0 @@
-Fluxo Normal:
-
-$ ./trab5 < program.cm
-
-Gera código Assembly:
-
-$ ./trab5 -d < program.cm
--- T5/wanderson/porting/interpreter/interpreter.h
+++
@@ -1,8 +0,0 @@
-#ifndef INTERPRETER_H
-#define INTERPRETER_H
-
-#include "../../ast/ast.h"
-
-void run_ast(AST *ast);
-
-#endif
